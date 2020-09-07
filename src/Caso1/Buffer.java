b37d5329878df8fa7559df39cd1b419782ace1fe
Stephania Otalora <s.otalorag@uniandes.edu.co>
2020-09-07 17:07:33 -0500

    Buffer

--- src/Caso1/Buffer.java
+++ src/Caso1/Buffer.java
@@ -1,5 +1,41 @@
 package Caso1;
 
+import java.util.ArrayList;
+
 public class Buffer {
+
+	private ArrayList buff	;
+	private int capacidad;
+	Object lleno, vacio;
+
+	public Buffer(int capacidad)
+	{
+		this.capacidad=capacidad;
+		buff= new ArrayList();
+		lleno= new Object ();
+		vacio= new Object ();
+	}
+	public void enviarMensaje(Mensaje mensaje) {
+		// TODO Auto-generated method stub
+		synchronized (lleno) {
+			while(buff.size()==capacidad)
+			{
+				try {
+					lleno.wait();
+				} catch (Exception e) {}
+			}
+
+		}
+
+		synchronized (this) { 
+			buff.add(mensaje);			
+		}
+
+		synchronized (vacio) {
+			vacio.notify();
+
+		}
+
+	}
 
 }
--- src/Caso1/Cliente.java
+++ src/Caso1/Cliente.java
@@ -1,20 +1,26 @@
 package Caso1;
 
 public class Cliente extends Thread{
-	
+
 	private int numMensajes;
-	
-	private Buffer buff;
-	
+
+	public static Buffer buff;
+
+	private Mensaje mensaje;
+
+
 	public Cliente(int numMensajes, Buffer buff) {
 		this.numMensajes=numMensajes;
 		this.buff=buff;
+		this.mensaje= null;
 	}
-	
+
 	public void run() {		
 		for(int i = 0; i<numMensajes; i++)
 		{
-			//crearMensaje(i);
+			mensaje= new Mensaje();
+
+
 		}
 	}
 
