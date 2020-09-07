450f8b7954c46e6ad117f47ab2e00b86f75641ec
Stephania Otalora <s.otalorag@uniandes.edu.co>
2020-09-07 16:43:00 -0500

    Cliente

--- src/Caso1/Cliente.java
+++ src/Caso1/Cliente.java
@@ -1,5 +1,21 @@
 package Caso1;
 
-public class Cliente {
+public class Cliente extends Thread{
+	
+	private int numMensajes;
+	
+	private Buffer buff;
+	
+	public Cliente(int numMensajes, Buffer buff) {
+		this.numMensajes=numMensajes;
+		this.buff=buff;
+	}
+	
+	public void run() {		
+		for(int i = 0; i<numMensajes; i++)
+		{
+			//crearMensaje(i);
+		}
+	}
 
 }
