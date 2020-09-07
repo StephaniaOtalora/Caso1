e8bbb437bd2980ae0be3e1bd932dfeeb82bd9f69
cristhian <cristhianforiguad@gmail.com>
2020-09-07 17:08:54 -0500

    Clase Servidor

--- src/Caso1/Mensaje.java
+++ src/Caso1/Mensaje.java
@@ -1,5 +1,18 @@
 package Caso1;
 
 public class Mensaje {
-
+	
+	private int mensaje; 
+	
+	public Mensaje(int pMensaje) {
+		this.mensaje = pMensaje; 
+	}
+	
+	private int getContenido() {
+		return this.mensaje; 
+	}
+	
+	private void responder() {
+		mensaje ++;
+	}
 }
--- src/Caso1/Servidor.java
+++ src/Caso1/Servidor.java
@@ -1,5 +1,18 @@
 package Caso1;
 
-public class Servidor {
+public class Servidor extends Thread{
+	
+	private static Buffer buff; 
+	
+	public Servidor(Buffer pBuff) {
+		this.buff = pBuff; 
+	}
+	
+	public void run() {
+		while(buff.darTotalClientes()) {
+			buff.responderMensaje();
+		}
+	}
+	
 
 }
