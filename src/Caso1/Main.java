9fa0943127a8a22161901f3b2d63b959c920f249
Stephania Otalora <s.otalorag@uniandes.edu.co>
2020-09-12 09:50:24 -0500

    Final

--- src/Caso1/Main.java
+++ src/Caso1/Main.java
@@ -1,4 +1,8 @@
 package Caso1;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 /**
  * 
  * @author Stephania Otalora
@@ -8,30 +12,49 @@ package Caso1;
 public class Main {
 
 	/**
-	 * MÈtodo main del proyecto. Se encarga de inicializar los threads de los clientes y de los servidores. AsÌ mismo, 
+	 * M√©todo main del proyecto. Se encarga de inicializar los threads de los clientes y de los servidores. As√≠ mismo, 
 	 * inicializa el buffer que comparte tanto cliente como servidor. El cual es el mismo tanto para clientes como servidores.
+	 * Inicializa todas las caracter√≠sticas de los objetos segun las especificaciones del archivo "configuracion.properties"
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		int capacidad = 6;
-		int pTotales = 3;
-		int servidores = 4;
-		
-		Buffer buffer = new Buffer(capacidad, pTotales);
-		
-		
-		for(int i=0; i<pTotales;i++)
+	public static void main(String[] args) {		
+
+
+		try{
+			Properties configuracion = new Properties();
+
+			FileInputStream input = new FileInputStream( "./data/configuracion.properties" );
+
+			configuracion.load( input );
+
+			int pTotales = Integer.parseInt(configuracion.getProperty("numero.clientes"));
+			int servidores = Integer.parseInt(configuracion.getProperty("numero.servidores"));						
+			int capacidad = Integer.parseInt(configuracion.getProperty("numero.capacidad"));
+			int mensajes = Integer.parseInt(configuracion.getProperty("numero.mensajes"));
+
+
+			Buffer buffer = new Buffer(capacidad, pTotales);
+
+			for(int i=0; i<pTotales;i++)
+			{
+				int numMensajes = (int) (Math.random()*mensajes+1);
+				Cliente c = new Cliente(numMensajes, buffer, i);
+				c.start();
+			}
+
+
+			for (int i = 0; i < servidores; i++) {
+				Servidor s= new Servidor(buffer, i);
+				s.start();
+			}
+			
+			System.out.println("Archivo cargado con exito.");
+
+		}
+		catch(Exception e)
 		{
-			int numMensajes = (int) (Math.random()*10+1);
-			Cliente c = new Cliente(numMensajes, buffer, i);
-			c.start();
+			System.out.println("Error al cargar la configuracion del programa.");
 		}
-		
-		for (int i = 0; i < servidores; i++) {
-			Servidor s= new Servidor(buffer, i);
-			s.start();
-		}
-		
 	}
 
 }
